d74db0faf0dee5d47d45c94f6391e7597e0f4e99
johnk2022 <keenan.john@hotmail.com>
2022-09-09 11:06:47 +0000

    Disable email authentication

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,9 +26,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["codestarjk2022.herokuapp.com", "localhost"]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
